f2f72e9fcf6825d6dcad43391d37e52b2f4e43d3
programmer-alijameel2020 <68697982+programmer-alijameel2020@users.noreply.github.com>
2021-12-01 11:52:28 +0300

    --initial layout

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,248 @@
+<template>
+  <v-list dense right class="mt-2 pa-md-2 px-md-3 pa-0 text-left transparent">
+    <v-list-item-group v-model="selectedItem">
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        class="mb-2"
+        :to="item.route"
+        color="blue darken-3"
+      >
+        <v-icon v-text="item.icon" small></v-icon>
+        <v-list-item-content class="text-left">
+          <v-list-item-title
+            v-text="item.title"
+            class="text-left ps-2 list-item-style d-none d-md-block"
+          ></v-list-item-title>
+        </v-list-item-content>
+        <!--      <v-list-item-icon></v-list-item-icon> -->
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
+</template>
+<style lang="scss" scopped>
+.list-item-style {
+  font-size: 14px !important;
+  font-weight: normal !important;
+}
+</style>
+<script>
+import { /* mapGetters ,*/ mapActions } from "vuex";
+
+export default {
+  name: "AppSideBar",
+  role: "",
+  data() {
+    return {
+      drawer: true,
+      selectedItem: 0,
+      items: [
+        {
+          title: "Main Dashboard",
+          icon: "mdi-home",
+          route: "/user/dashboard/main",
+        },
+        {
+          title: "Your Videos",
+          icon: "mdi-video",
+          route: "/user/dashboard/new-record",
+        },
+
+        {
+          title: "Favourite",
+          icon: "mdi-heart",
+          route: "/user/dashboard/display-data",
+        },
+
+        {
+          title: "Linked Accounts",
+          icon: "mdi-account-multiple",
+          route: "/user/dashboard/report-data",
+        },
+        {
+          title: "Video Studio",
+          icon: "mdi-pencil",
+          route: "/user/dashboard/user-activity",
+        },
+        {
+          title: "Settings",
+          icon: "mdi-cog",
+          route: "/user/dashboard/user-settings",
+        },
+      ],
+      mini: false,
+    };
+  },
+
+  computed: {
+    /* ...mapGetters(["authenticated"]), */
+  },
+  methods: {
+    ...mapActions(["signout"]),
+    roleCheck(role) {
+      if (role == "superadmin") {
+        this.items = [
+          {
+            title: "الرئيسية",
+            icon: "mdi-home",
+            route: "/user/dashboard/main",
+          },
+          {
+            title: "شكاوي التنظيم الاعلامي",
+            icon: "mdi-satellite-uplink",
+            route: "/user/dashboard/complaint",
+          },
+          {
+            title: "شكاوي  تنظيم الاتصالات",
+            icon: "mdi-transmission-tower",
+            route: "/user/dashboard/complaint-communications",
+          },
+          {
+            title: "أرشيف الرسائل النصية SMS",
+            icon: "mdi-message-alert-outline",
+            route: "/user/dashboard/sms-archive",
+          },
+          {
+            title: "التقارير",
+            icon: "mdi-poll",
+            route: "/user/dashboard/userReports",
+          },
+          {
+            title: "معلومات الحساب",
+            icon: "mdi-file-account",
+            route: "/user/dashboard/userInfo",
+          },
+        ];
+      } else if (role == "media") {
+        this.items = [
+          {
+            title: "الرئيسية",
+            icon: "mdi-desktop-mac-dashboard",
+            route: "/user/dashboard/main",
+          },
+          {
+            title: "شكاوي التنظيم الاعلامي",
+            icon: "mdi-file-document",
+            route: "/user/dashboard/complaint",
+          },
+          {
+            title: "معلومات الحساب",
+            icon: "mdi-file-account",
+            route: "/user/dashboard/userInfo",
+          },
+        ];
+      } else if (role == "voc") {
+        this.items = [
+          {
+            title: "الرئيسية",
+            icon: "mdi-desktop-mac-dashboard",
+            route: "/user/dashboard/main",
+          },
+          {
+            title: "شكاوي  تنظيم الاتصالات",
+            icon: "mdi-file-document",
+            route: "/user/dashboard/complaint-communications",
+          },
+          {
+            title: "قائمة الارقام الممنوعة",
+            icon: "mdi-file-cancel",
+            route: "/user/dashboard/block-list",
+          },
+          {
+            title: "أرشيف الرسائل النصية SMS",
+            icon: "mdi-message-alert-outline",
+            route: "/user/dashboard/sms-archive",
+          },
+          {
+            title: "التقارير",
+            icon: "mdi-poll",
+            route: "/user/dashboard/userReports",
+          },
+          {
+            title: "معلومات الحساب",
+            icon: "mdi-file-account",
+            route: "/user/dashboard/userInfo",
+          },
+        ];
+      } else if (role == "zain") {
+        this.items = [
+          {
+            title: "الرئيسية",
+            icon: "mdi-desktop-mac-dashboard",
+            route: "/user/dashboard/main",
+          },
+          {
+            title: "شكاوي  تنظيم الاتصالات",
+            icon: "mdi-file-document",
+            route: "/user/dashboard/complaint-communications",
+          },
+          {
+            title: "التقارير",
+            icon: "mdi-poll",
+            route: "/user/dashboard/userReports",
+          },
+          {
+            title: "معلومات الحساب",
+            icon: "mdi-file-account",
+            route: "/user/dashboard/userInfo",
+          },
+        ];
+      } else if (role == "asiacell") {
+        this.items = [
+          {
+            title: "الرئيسية",
+            icon: "mdi-desktop-mac-dashboard",
+            route: "/user/dashboard/main",
+          },
+          {
+            title: "شكاوي  تنظيم الاتصالات",
+            icon: "mdi-file-document",
+            route: "/user/dashboard/complaint-communications",
+          },
+          {
+            title: "التقارير",
+            icon: "mdi-poll",
+            route: "/user/dashboard/userReports",
+          },
+          {
+            title: "معلومات الحساب",
+            icon: "mdi-file-account",
+            route: "/user/dashboard/userInfo",
+          },
+        ];
+      } else if (role == "korek") {
+        this.items = [
+          {
+            title: "الرئيسية",
+            icon: "mdi-desktop-mac-dashboard",
+            route: "/user/dashboard/main",
+          },
+          {
+            title: "شكاوي  تنظيم الاتصالات",
+            icon: "mdi-file-document",
+            route: "/user/dashboard/complaint-communications",
+          },
+          {
+            title: "التقارير",
+            icon: "mdi-poll",
+            route: "/user/dashboard/userReports",
+          },
+          {
+            title: "معلومات الحساب",
+            icon: "mdi-file-account",
+            route: "/user/dashboard/userInfo",
+          },
+        ];
+      } else {
+        this.items = [];
+      }
+    },
+  },
+
+  created() {
+    /* this.fullname = this.authenticated.fullname;
+    this.role = this.authenticated.role;
+    this.roleCheck(this.role); */
+  },
+};
+</script>
--- src/views/User/Main.vue
+++ src/views/User/Main.vue
@@ -2,7 +2,14 @@
   <div class="main-container">
     <UpperDivision />
     <v-container :fluid="fluidState" class="contain-body mt-1">
-      <router-view />
+      <v-row>
+        <v-col cols="12" class="sidebar">
+          <Sidebar />
+        </v-col>
+        <v-col cols="12" md="10" sm="12" lg="10">
+          <router-view />
+        </v-col>
+      </v-row>
     </v-container>
     <div class="footer-height">
       <Footer />
@@ -13,11 +20,13 @@
 <script>
 import UpperDivision from "../../components/UpperDivision";
 import Footer from "../../components/Footer";
+import Sidebar from "../../components/Sidebar.vue";
 export default {
   name: "contentPage",
   components: {
     UpperDivision,
     Footer,
+    Sidebar,
   },
   data() {
     return {
@@ -43,6 +52,10 @@ export default {
 </script>
 
 <style lang="scss" scopped>
+.sidebar {
+  max-width: 10%;
+}
+
 .footer-height {
   height: 5vh !important;
 }
@@ -50,6 +63,18 @@ export default {
 .contain-body {
   height: 85vh !important;
   overflow-y: auto !important;
+}
+
+@media only screen and (max-width: 1300px) {
+  .sidebar {
+    max-width: 20%;
+  }
+}
+
+@media only screen and (max-width: 1026px) {
+  .sidebar {
+    max-width: 20%;
+  }
 }
 
 @media only screen and (max-width: 600px) {
